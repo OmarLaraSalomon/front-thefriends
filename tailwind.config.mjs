7da78a22a0abbcbee05835a42077bd84f8ff991f
omar-lara <omar.laso0001@gmail.com>
2024-09-03 13:25:34 -0600

    primera landing-aprobada

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+import animations from '@midudev/tailwind-animations'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkmode: 'class',
+	theme: {
+		extend: {
+			
+		},
+	},
+	plugins: [animations],
+}
